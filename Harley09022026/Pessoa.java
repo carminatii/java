2cadb4a7f2395245ed2ca3d9a0106ad4aae6ed3a
Harley Carminati <carminatiharley@gmail.com>
2026-02-18 16:29:37 -0300

    Add files via upload

---
+++ Harley09022026/Pessoa.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.harley09022026;
+
+/**
+ *
+ * @author alunolab13
+ */
+public class Pessoa {
+    String nome;
+    int idade;
+    
+    String imprimir(){
+        return "Meu nome é " + nome + " e minha idade é " + idade + " anos."; 
+    }
+    
+}
